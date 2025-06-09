d7b4ca0d04c4002cd77d161fc3eed6964ee98cad
Jesus Tarazona S <rjtarazonas97@gmail.com>
2025-06-09 17:51:27 -0500

    add folder basic and exercise

---
+++ 001_basic/excercise.py
@@ -0,0 +1,61 @@
+###
+# exercises.py
+# Ejercicios para practicar los conceptos aprendidos en las lecciones.
+###
+
+from os import system
+if system("clear") != 0: system("cls")
+
+print("\nEjercicio 1: Imprimir mensajes")
+print("Escribe un programa que imprima tu nombre y tu ciudad en líneas separadas.")
+
+### Completa aquí
+
+print("Jesus tarazona\nsoy de la ciudad de lima")
+
+print("\nEjercicio 2: Muestra los tipos de datos de las siguientes variables:")
+print("Usa el comando 'type()' para determinar el tipo de datos de cada variable.")
+a = 15
+b = 3.14159
+c = "Hola Mundo"
+d = True
+e = None
+
+### Completa aquí
+
+a = print(type(15))
+b = print(type(3.14159))
+c = print(type("Hola mundo"))
+d = print(type(True))
+e = print(type(None))
+
+print("\nEjercicio 3: Casting de tipos")
+print("Convierte la cadena \"12345\" a un entero y luego a un float.")
+print("Convierte el float 3.99 a un entero. ¿Qué ocurre?")
+
+### Completa aquí
+
+print(int("12345\n"))
+print(float("12345"))
+print(int(3.99))
+
+print("\nEjercicio 4: Variables")
+print("Crea variables para tu nombre, edad y altura.")
+print("Usa f-strings para imprimir una presentación.")
+
+# "Hola! Me llamo midudev y tengo 39 años, mido 1.70 metros"
+
+### Completa aquí
+name = input("Ingresa tu nombre\n")
+age = input("Ingresa tu edad\n")
+heigth = input("Ingresa tu estatura\n")
+
+print(f"Hola me llamo{name} y tengo {age} años, mido {heigth} metros")
+
+print("\nEjercicio 5: Números")
+print("1. Crea una variable con el número PI (sin asignar una variable)")
+print("2. Redondea el número con round()")
+print("3. Haz la división entera entre el número que te salió y el número 2")
+print("4. El resultado debería ser 1")
+
+print(int(round(3.14159)/2))
